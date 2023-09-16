bcb45ffb2428a3f77b34dce957fe586a604dea69
mohamed-bo <medprgrm@gmail.com>
2023-09-17 00:11:24 +0100

    printf

--- print_normalFlags2.c
+++ print_normalFlags2.c
@@ -77,49 +77,10 @@ int print_number(char *s, format_fg *flagPar)
 	if (isNegative)
 		*--s = '-';
 
-	return (print_number_left_shift(s, flagPar));
-}
-
-
-
-/**
- * print_number_left_shift - prints a number flag minus
- * @s: the number
- * @flagPar: the parameter of format
- * Return: number of char printed
- */
-int print_number_left_shift(char *s, format_fg *flagPar)
-{
-	unsigned int counter = 0, i = _strlen(s);
-	unsigned int isNegative = (!flagPar->unsign && *s == '-');
-
 	if (!flagPar->minus)
-	{
-		while (i++ < flagPar->width)
-			counter += _putchar(' ');
-		if (!isNegative && !flagPar->unsign)
-		{
-		if (flagPar->plus)
-			counter += _putchar('+'), i++;
-		else if (flagPar->space)
-			counter += _putchar(' '), i++;
-		}
-		counter += _putString(s);
-	}
+		return (print_number_right_shift(s, flagPar));
 	else
-	{
-		if (!isNegative && !flagPar->unsign)
-		{
-		if (flagPar->plus)
-			counter += _putchar('+'), i++;
-		else if (flagPar->space)
-			counter += _putchar(' '), i++;
-		}
-		counter += _putString(s);
-		while (i++ < flagPar->width)
-			counter += _putchar(' ');
-	}
-	return (counter);
+		return (print_number_left_shift(s, flagPar));
 }
 
 /**
@@ -160,4 +121,28 @@ int print_number_right_shift(char *s, format_fg *flagPar)
 		n += _putchar(' ');
 	n += _putString(s);
 	return (n);
+}
+
+/**
+ * print_number_left_shift - prints a number flag minus
+ * @s: the number
+ * @flagPar: the parameter of format
+ * Return: number of char printed
+ */
+int print_number_left_shift(char *s, format_fg *flagPar)
+{
+	unsigned int counter = 0, i = _strlen(s);
+	unsigned int isNegative = (!flagPar->unsign && *s == '-');
+
+	if (!isNegative && !flagPar->unsign)
+	{
+	if (flagPar->plus)
+		counter += _putchar('+'), i++;
+	else if (flagPar->space)
+		counter += _putchar(' '), i++;
+	}
+	counter += _putString(s);
+	while (i++ < flagPar->width)
+		counter += _putchar(' ');
+	return (counter);
 }
