4fad3dba4d33a6e376ab0b70f4b1fdb21aacfd55
mohamed-bo <medprgrm@gmail.com>
2023-09-16 17:57:09 +0100

    printf

--- flag.c
+++ flag.c
@@ -3,7 +3,6 @@
 /**
  * init_params - clear the paramter and reset buffer
  * @flagPar: the parameter of format
- * @agrument: the argument pointer
  */
 void init_params(format_fg *flagPar)
 {
@@ -27,20 +26,20 @@ void init_params(format_fg *flagPar)
  * get_flag_param - set the paramter of flag
  * @s: the format string
  * @flagPar: the parameter of format
- * @agrument: the argument pointer
+ * @agruments: the argument pointer
  * Return: current s pointer
  */
 char *get_flag_param(char *s, format_fg *flagPar, va_list agruments)
 {
 	while (get_flag(s, flagPar))
-			{
-				s++;
-			}
+	{
+		s++;
+	}
 	s = get_width(s, flagPar, agruments);
 	s = get_precision(s, flagPar, agruments);
 	s = get_modifier(s, flagPar);
 	get_specifier(s, flagPar);
-	return(s);
+	return (s);
 }
 
 
@@ -74,7 +73,6 @@ int get_flag(char *s, format_fg *flagPar)
 	}
 	return (isFlag);
 }
-
 
 /**
  * get_width - gets the width
@@ -127,64 +125,4 @@ char *get_precision(char *s, format_fg *flagPar, va_list agrument)
 	}
 	flagPar->precision = precision;
 	return (s);
-}
-
-/**
- * get_modifier - get the modifier type
- * @s: the format string
- * @flagPar: the parameter of format
- * Return: current s pointer
- */
-char *get_modifier(char *s, format_fg *flagPar)
-{
-	switch (*s)
-	{
-	case 'h':
-		flagPar->h_mod = 1;
-		s++;
-		break;
-	case 'l':
-		flagPar->l_mod = 1;
-		s++;
-		break;
-	}
-	return (s);
-}
-
-/**
- * get_specifier - get the type of specifier
- * @s: the format string
- * Return: 1 if there format
- * else 0
- */
-int get_specifier(char *s, format_fg *flagPar)
-{
-	format_specifier specifiers[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_int},
-		{"i", print_int},
-		{"b", print_bin},
-		{"o", print_oct},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"u", print_unsigned},
-		{"p", print_address},
-		{"r", print_rev},
-		{"R", print_rot13},
-		{"S", print_stringNonPrintable},
-		{NULL, NULL}
-	};
-	int i = 0;
-	while (specifiers[i].specifier)
-	{
-		if (*s == specifiers[i].specifier[0])
-		{
-			(*flagPar).specifier = specifiers[i].func;
-			return (1);
-		}
-		i++;
-	}
-	return (0);
 }
--- print_utils.c
+++ print_utils.c
@@ -58,7 +58,7 @@ int print_rev(va_list agrument, format_fg *flagPar)
 	int i, counter = 0;
 	char *s = va_arg(agrument, char *);
 
-    (void)flagPar;
+	(void)flagPar;
 	if (s)
 	{
 		for (i = 0; *s; s++)
@@ -100,95 +100,4 @@ int print_rot13(va_list agrument, format_fg *flagPar)
 		i++;
 	}
 	return (count);
-}
-
-/**
- * print_number - prints a number
- * @s: the number
- * @flagPar: the parameter of format
- * Return: number of char printed
- */
-int print_number(char *s, format_fg *flagPar)
-{
-	unsigned int i = _strlen(s);
-	int signn = (!flagPar->unsign && *s == '-');
-
-	if (!flagPar->precision && *s == '0' && !s[1])
-		s = "";
-	if (signn)
-	{
-		s++;
-		i--;
-	}
-	if (flagPar->precision != UINT_MAX)
-		while (i++ < flagPar->precision)
-			*--s = '0';
-	if (signn)
-		*--s = '-';
-
-	if (!flagPar->minus)
-		return (print_number_right_shift(s, flagPar));
-	else
-		return (print_number_left_shift(s, flagPar));
-}
-
-/**
- * print_number_right_shift - prints a number flag non minus
- * @s: the number
- * @flagPar: the parameter of format
- * Return: number of char printed
- */
-int print_number_right_shift(char *s, format_fg *flagPar)
-{
-	unsigned int n = 0, isNegative, i = _strlen(s);
-	char paddingChar = flagPar->zero ? '0' : ' ';
-    unsigned int signn;
-
-	signn = isNegative = (!flagPar->unsign && *s == '-');
-	if (signn && i < flagPar->width && flagPar->zero)
-		s++;
-	else
-		signn = 0;
-	if ((flagPar->plus && !isNegative) ||
-		(!flagPar->plus && flagPar->space && !isNegative))
-		i++;
-	if (signn && flagPar->zero)
-		n += _putchar('-');
-	if (flagPar->plus && !isNegative && flagPar->zero && !flagPar->unsign)
-		n += _putchar('+');
-	else if (!flagPar->plus && flagPar->space && !isNegative &&
-		!flagPar->unsign && flagPar->zero)
-		n += _putchar(' ');
-	while (i++ < flagPar->width)
-		n += _putchar(paddingChar);
-	if (signn && !flagPar->zero)
-		n += _putchar('-');
-	if (flagPar->plus && !isNegative && !flagPar->zero && !flagPar->unsign)
-		n += _putchar('+');
-	else if (!flagPar->plus && flagPar->space && !isNegative &&
-		!flagPar->unsign && !flagPar->zero)
-		n += _putchar(' ');
-	n += _puts(s);
-	return (n);
-}
-
-/**
- * print_number_left_shift - prints a number flag minus
- * @s: the number
- * @flagPar: the parameter of format
- * Return: number of char printed
- */
-int print_number_left_shift(char *s, format_fg *flagPar)
-{
-	unsigned int n = 0, isNegative, i = _strlen(s);
-
-	isNegative = (!flagPar->unsign && *s == '-');
-	if (flagPar->plus && !isNegative && !flagPar->unsign)
-		n += _putchar('+'), i++;
-	else if (flagPar->space && !isNegative && !flagPar->unsign)
-		n += _putchar(' '), i++;
-	n += _puts(s);
-	while (i++ < flagPar->width)
-		n += _putchar(' ');
-	return (n);
 }
