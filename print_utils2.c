17959ecd9fbcc8de5458aa3a9802712f8f727143
mohamed-bo <medprgrm@gmail.com>
2023-09-16 19:43:56 +0100

    printf

--- print_utils2.c
+++ print_utils2.c
@@ -7,7 +7,7 @@
  *
  * Return: chars printed
  */
-int print_number(char *str, params_t *params)
+int print_number(char *str, format_fg *params)
 {
 	unsigned int i = _strlen(str);
 	int neg = (!params->unsign && *str == '-');
@@ -25,7 +25,7 @@ int print_number(char *str, params_t *params)
 	if (neg)
 		*--str = '-';
 
-	if (!params->minus_flag)
+	if (!params->minus)
 		return (print_number_right_shift(str, params));
 	else
 		return (print_number_left_shift(str, params));
@@ -38,36 +38,36 @@ int print_number(char *str, params_t *params)
  *
  * Return: chars printed
  */
-int print_number_right_shift(char *str, params_t *params)
+int print_number_right_shift(char *str, format_fg *params)
 {
 	unsigned int n = 0, neg, neg2, i = _strlen(str);
 	char pad_char = ' ';
 
-	if (params->zero_flag && !params->minus_flag)
+	if (params->zero && !params->minus)
 		pad_char = '0';
 	neg = neg2 = (!params->unsign && *str == '-');
-	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+	if (neg && i < params->width && pad_char == '0' && !params->minus)
 		str++;
 	else
 		neg = 0;
-	if ((params->plus_flag && !neg2) ||
-		(!params->plus_flag && params->space_flag && !neg2))
+	if ((params->plus && !neg2) ||
+		(!params->plus && params->space && !neg2))
 		i++;
 	if (neg && pad_char == '0')
 		n += _putchar('-');
-	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
+	if (params->plus && !neg2 && pad_char == '0' && !params->unsign)
 		n += _putchar('+');
-	else if (!params->plus_flag && params->space_flag && !neg2 &&
-		!params->unsign && params->zero_flag)
+	else if (!params->plus && params->space && !neg2 &&
+		!params->unsign && params->zero)
 		n += _putchar(' ');
 	while (i++ < params->width)
 		n += _putchar(pad_char);
 	if (neg && pad_char == ' ')
 		n += _putchar('-');
-	if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
+	if (params->plus && !neg2 && pad_char == ' ' && !params->unsign)
 		n += _putchar('+');
-	else if (!params->plus_flag && params->space_flag && !neg2 &&
-		!params->unsign && !params->zero_flag)
+	else if (!params->plus && params->space && !neg2 &&
+		!params->unsign && !params->zero)
 		n += _putchar(' ');
 	n += _puts(str);
 	return (n);
@@ -80,22 +80,22 @@ int print_number_right_shift(char *str, params_t *params)
  *
  * Return: chars printed
  */
-int print_number_left_shift(char *str, params_t *params)
+int print_number_left_shift(char *str, format_fg *params)
 {
 	unsigned int n = 0, neg, neg2, i = _strlen(str);
 	char pad_char = ' ';
 
-	if (params->zero_flag && !params->minus_flag)
+	if (params->zero && !params->minus)
 		pad_char = '0';
 	neg = neg2 = (!params->unsign && *str == '-');
-	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+	if (neg && i < params->width && pad_char == '0' && !params->minus)
 		str++;
 	else
 		neg = 0;
 
-	if (params->plus_flag && !neg2 && !params->unsign)
+	if (params->plus && !neg2 && !params->unsign)
 		n += _putchar('+'), i++;
-	else if (params->space_flag && !neg2 && !params->unsign)
+	else if (params->space && !neg2 && !params->unsign)
 		n += _putchar(' '), i++;
 	n += _puts(str);
 	while (i++ < params->width)
