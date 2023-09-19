61d45528818aa22d428e191e04d15a4755cfbdd6
Wiwicha <wiam.silabdi.eln@gmail.com>
2023-09-19 13:48:54 -0700

    wiam

---
+++ convert.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * convert - converter function
+ * @number: number in decimal
+ * @base: base
+ * @flags: argument flags
+ * Return: result in string
+ */
+char *convert(long int number, int base, int flags)
+{
+	static char *array;
+	static char buffer[1024];
+	char sign = 0;
+	char *current;
+	unsigned long num = number;
+
+	array = flags & LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	if (!(flags & UNSIGNED) && number < 0)
+	{
+		num = -number;
+		sign = '-';
+
+	}
+	current = &buffer[1023];
+	*current = '\0';
+	do	{
+		*--current = array[num % base];
+		num /= base;
+	} while (num != 0);
+
+	if (sign)
+		*--current = sign;
+	return (current);
+}
